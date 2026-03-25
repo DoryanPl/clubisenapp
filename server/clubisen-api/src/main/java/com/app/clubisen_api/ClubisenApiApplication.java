c938f4b33564d3accf8e94a8c74dc706322cc718
Doryan <doryan.planchon@hotmail.fr>
2026-03-25 23:24:31 +0100

    init backend

---
+++ server/clubisen-api/src/main/java/com/app/clubisen_api/ClubisenApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.clubisen_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubisenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubisenApiApplication.class, args);
+	}
+
+}
---
+++ server/clubisen-api/src/test/java/com/app/clubisen_api/ClubisenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.clubisen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubisenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
